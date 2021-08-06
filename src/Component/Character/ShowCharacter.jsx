6e447d3d993c173e516289f836673ab79815487d
JennyViannay <jenny.viannay75@gmail.com>
2021-08-06 17:23:29 +0200

    add episodes list by characters on show one character, page still in progress

--- src/Component/Character/ShowCharacter.jsx
+++ src/Component/Character/ShowCharacter.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
+import EpisodeUl from '../Episode/EpisodeUl';
+
 const ShowCharacter = () => {
     const params = useParams();
     const [character, getCharacter] = useState();
@@ -16,14 +18,19 @@ const ShowCharacter = () => {
         <div className="container">
             {
                 character ? (
-                    <div className="row">
-                        <div className="col-lg-4 col-md-4 col-xs-12">
-                            <img src={character.image} alt="" className="mx-auto rounded" />
+                    <>
+                        <div className="row">
+                            <div className="col-lg-4 col-md-4 col-xs-12">
+                                <img src={character.image} alt="" className="mx-auto rounded" />
+                            </div>
+                            <div className="col-lg-8 col-md-6 col-xs-12">
+                                <h1>{character.name}</h1>
+                            </div>
                         </div>
-                        <div className="col-lg-8 col-md-6 col-xs-12">
-                            <h1>{character.name}</h1>
+                        <div className="row text-center">
+                            <EpisodeUl episodes={character.episode} />
                         </div>
-                    </div>
+                    </>
                 ) : 'Loading...'
             }
         </div>
